829b68a6f4f24a952d3ca2d28bb5137b18b81d21
George <georgewitteman@gmail.com>
2015-11-01 16:10:30 -0500

    Test the key's

--- vulnerable_space/vulnerable_space/ViewController.swift
+++ vulnerable_space/vulnerable_space/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            print("Object has been saved.")
+        }
     }
 
     override func didReceiveMemoryWarning() {
